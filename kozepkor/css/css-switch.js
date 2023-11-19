e2db6a6046bd7eff3bf193a14f98a0f2d99502df
Liu Bálint <en0bmp@inf.elte.hu>
2023-11-19 17:25:41 +0100

    final

--- kozepkor/css/css-switch.js
+++ kozepkor/css/css-switch.js
@@ -1,23 +1,25 @@
 (function () {
     'use strict';
 
+    var main = document.getElementById('main') != null;
+    
   document.addEventListener('DOMContentLoaded', function () {
     var contrast = localStorage.getItem('high-contrast');
 
-    if (contrast) switchMode('high-contrast', 'sajat');
+    if (contrast) switchMode('high-contrast', 'sajat', main);
   });
 
   document.getElementById('activate-high-contrast').addEventListener('click', function () {
-    switchMode('high-contrast', 'sajat');
+    switchMode('high-contrast', 'sajat', main);
     localStorage.setItem('high-contrast', 'true');
   });
 
   document.getElementById('activate-normal').addEventListener('click', function () {
-    switchMode('sajat', 'high-contrast');
+    switchMode('sajat', 'high-contrast', main);
     localStorage.removeItem('high-contrast');
   });
 
-  function switchMode(newMode, prevMode) {
+  function switchMode(newMode, prevMode, main) {
     var link = document.querySelector('link[rel=stylesheet][href*="' + prevMode + '"]');
     link.parentNode.removeChild(link);
 
@@ -25,7 +27,11 @@
 
     link.type = 'text/css'
     link.rel = 'stylesheet'
-    link.href = 'css/' + newMode + '.css'
+    if(main){
+      link.href = 'css/' + newMode + '.css'
+    }else{
+      link.href = '../css/' + newMode + '.css'
+    }
 
     document.head.appendChild(link)
   }
